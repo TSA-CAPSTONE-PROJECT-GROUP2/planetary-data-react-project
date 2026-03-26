a6e4157321c503e42b48832c260f6e820b41241d
omowumi jeremiah <omowumijeremiah25@gmail.com>
2026-03-26 21:41:25 +0100

    planet

--- src/features/planets/components/PlanetCard.jsx
+++ src/features/planets/components/PlanetCard.jsx
@@ -18,16 +18,3 @@ function PlanetCard({name, distance, images}) {
   )
 }
 export default PlanetCard
-
-// export default function PlanetCard({ planet }) {
-//   return (
-//     <figure className="planet-card">
-//       {/* Mock image until data is fetched */}
-//       <img src="placeholder.jpg" alt="Planet placeholder" />
-//       <figcaption>
-//         <h3>Planet Name</h3>
-//         <p>Distance from Sun</p>
-//       </figcaption>
-//     </figure>
-//   );
-// }
